d275119e0c03b88226662772af9d8eec435f964c
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2025-05-22 14:10:20 +0300

    ORM

---
+++ orm.py
@@ -0,0 +1,49 @@
+from sqlalchemy import ForeignKey, create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import sessionmaker
+
+# Creating a DB Instance
+engine = create_engine("sqlite:///blog2.db")
+
+
+Base = declarative_base()
+#Creating the Tables
+
+class User(Base):
+    
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    
+    profile = relationship("Profile", back_populates="user")
+    posts= relationship("Post", back_populates="user")
+    
+class Profile(Base):
+    
+    __tablename__ = "profiles"
+    
+    id = Column(Integer, primary_key=True)
+    genre = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="profile")
+    
+class Post(Base):
+    
+    __tablename__ = "posts"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user =  relationship("User", back_populates="posts")
+    
+# Creating a Session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+test_user = User(name="Test User")
+test_profile = Profile(genre="Test Genre", user=test_user)
+test_post = Post(title="Test Post", content="Test Content", user=test_user)
+session.add_all([test_user, test_profile, test_post])
+session.commit()
